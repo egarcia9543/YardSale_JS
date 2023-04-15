0288ba55ebdf0ebeaf374763b9fbe5fef021dd9d
Esteban <egarcia9543@soy.sena.edu.co>
2023-04-15 13:09:01 -0500

    Se fusionó el menú mobile con el index

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".userEmail");
-const desktopMenu = document.querySelector(".desktopMenu")
+const desktopMenu = document.querySelector(".desktopMenu");
+
+const burgerMenu = document.querySelector(".burgerMenu");
+const mobileMenu = document.querySelector(".mobileMenu")
 
 menuEmail.addEventListener("click", toggleMenu);
+burgerMenu.addEventListener("click", toggleBurger);
 
 function toggleMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleBurger(){
+    mobileMenu.classList.toggle("inactive");
 }
