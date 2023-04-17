03e5a68dd17d920e549a90963a36d338c055d0ee
Esteban <egarcia9543@soy.sena.edu.co>
2023-04-16 23:19:59 -0500

    Cambios en el carrito

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const desktopMenu = document.querySelector(".desktopMenu");
 const burgerMenu = document.querySelector(".burgerMenu");
 const mobileMenu = document.querySelector(".mobileMenu")
 
+const cartMenu = document.querySelector(".navShoppingCart");
+const cartDetail = document.querySelector(".cartDetail");
+
 menuEmail.addEventListener("click", toggleMenu);
 burgerMenu.addEventListener("click", toggleBurger);
+cartMenu.addEventListener("click", toggleCart);
 
 function toggleMenu(){
     desktopMenu.classList.toggle("inactive");
@@ -13,4 +17,12 @@ function toggleMenu(){
 
 function toggleBurger(){
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCart(){
+    cartDetail.classList.toggle("inactive");
+
+    if (mobileMenu.classList.contains("inactive")) {
+        
+    }
 }
